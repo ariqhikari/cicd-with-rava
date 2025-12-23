67c7c4cfac5a3eda9d982623927f9211b6ba1e7b
ariqhikari <ahidayatbia@gmail.com>
2025-12-23 17:05:34 +0700

    fix: delete variable deleteUserByIdController

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { createUserController, getListUsersController, deleteUserByIdController } from "../controllers/userController.js";
+import {
+  createUserController,
+  getListUsersController,
+} from "../controllers/userController.js";
 
 const userRouter = Router();
 
-userRouter.post('/', createUserController);
-userRouter.get('/', getListUsersController);
+userRouter.post("/", createUserController);
+userRouter.get("/", getListUsersController);
 // userRouter.delete('/:userId', deleteUserByIdController);
 
-export {
-    userRouter
-}
+export { userRouter };
